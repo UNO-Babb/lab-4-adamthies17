88875e8b56b0cf783c76c0d03ebdad8c45321806
adamthies17 <adamthies17@gmail.com>
2025-02-16 16:06:12 -0600

    Done with all parts of lab

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -11,17 +11,68 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
-
+def drawPolygon(larry, sides):
+    for s in range(sides):
+        larry.forward(50)
+        larry.right(360/sides)
+        
+def fillCorner(gary, corner):
+    drawSquare(gary, 150)
+    if corner == 1:
+        gary.begin_fill()
+        drawSquare(gary, 75)
+        gary.end_fill()
+    elif corner == 2:
+        gary.forward(75)
+        gary.begin_fill()
+        drawSquare(gary, 75)
+        gary.end_fill()
+    elif corner == 4:
+        gary.right(90)
+        gary.forward(75)
+        gary.left(90)
+        gary.begin_fill()
+        drawSquare(gary, 75)
+        gary.end_fill()
+    elif corner == 3:
+        gary.right(90)
+        gary.forward(150)
+        gary.left(90)
+        gary.forward(75)
+        gary.left(90)
+        gary.begin_fill()
+        drawSquare(gary, 75)
+        gary.end_fill()
+        
+def squaresInSquares(gus, squares):
+    theSize = 20
+    for j in range(squares):
+        drawSquare(gus, theSize)
+        gus.penup()
+        gus.left(135)
+        gus.forward(20)
+        gus.right(135)
+        gus.pendown()
+        theSize= theSize + 29
+        
+        
+        
+        
+        
+        
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+    
+    
+   
+    #drawPolygon(myTurtle, 5) #draws a pentagon
+    #drawPolygon(myTurtle, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    #fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    #fillCorner(myTurtle, 4) #draws a square bottom left corner filled in.
 
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    #squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+    squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
 main()
